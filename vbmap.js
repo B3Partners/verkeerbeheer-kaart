265b1cdda6f3935b9779a5fc565ae15c03a77554
Meine Toonen <toonen.meine@gmail.com>
2015-12-24 11:19:25 +0100

    TMS working, arcgis layer not correctly working, but it's WMS is.

--- vbmap.js
+++ vbmap.js
@@ -9,12 +9,45 @@ var RD2 = new L.Proj.CRS.TMS(
     });
 
 
-var map = L.map('map', {crs:RD2}).setView([52.5, 5.8], 2);
+var map = L.map('map', 
+	{
+		crs:RD2
+	}).setView([52.5, 5.8], 3);
 /*
 L.tileLayer('http://geodata.nationaalgeoregister.nl/tms/1.0.0/brtachtergrondkaart/{z}/{x}/{y}.png', {
     tms: true
-}).addTo(map);*/
-
-L.esri.DynamicMapLayer("http://geo.flevoland.nl/arcgis/rest/services/Groen_Natuur/Agrarische_Natuur/MapServer", {
-    "layers": "0"
 }).addTo(map);
+*/
+L.tileLayer('http://www.openbasiskaart.nl/mapcache/tms/1.0.0/osm@rd/{z}/{x}/{y}.png', {
+    tms: true,
+    minZoom: 3,
+	opacity: 0.5,
+    maxZoom: 13,
+    continuousWorld: true
+}).addTo(map);
+/*
+L.tileLayer.wms("http://geo.flevoland.nl/arcgis/services/Groen_Natuur/Agrarische_Natuur/MapServer/WMSServer",{
+	layers:['0', '1', '2', '3', '4'],
+	transparent: true,
+	format: 'iamge/png'
+}).addTo(map);
+*/
+/*
+L.esri.dynamicMapLayer({
+	url: "http://geo.flevoland.nl/arcgis/rest/services/Groen_Natuur/Agrarische_Natuur/MapServer",
+    "layers": ["0"],
+    imageSR: 28992
+}).addTo(map);
+
+*/
+/*L.esri.dynamicMapLayer({
+	url: "http://services.arcgisonline.nl/arcgis/rest/services/Basiskaarten/Stratenkaart/MapServer",
+	opacity: 0.5
+  // "layers": ["0"]
+}).addTo(map);
+*/
+/*L.esri.dynamicMapLayer({
+	url: "http://ags101.prvgld.nl/arcgis/rest/services/Algemeen/Ondergrond_grijs/MapServer",
+    "layers": ["0"],
+    transparent: true
+}).addTo(map);*/
