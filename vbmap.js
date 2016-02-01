5d9e7745cc417058f5d63528aefa932ba7439e4b
Meine Toonen <toonen.meine@gmail.com>
2016-02-01 10:19:55 +0100

    Last commit before switching to OL3

--- vbmap.js
+++ vbmap.js
@@ -31,21 +31,7 @@ function vbmap(){
 	 * @params config Configuration object. See the header in config.json for the specification of the file.
 	 */
 	this.init = function (config){
-		var res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420,0.210,0.105,0.052,0.025,0.012,0.006,0.003];
-	
-		//var res = [3440.6399,1720.3199,860.1599,430.0799,215.0399,107.5199,53.7599,26.8799,13.4399,6.7199,3.3599,1.6799,0.8400,0.4200,0.2100,0.1050,0.0525,0.0262,0.0131,0.0065,0.0032,0.0016];
-		var rd = new L.Proj.CRS.TMS(
-	        'EPSG:28992',
-	        '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999], 
-	    {
-	            transformation: new L.Transformation(1,0,-1,0),
-	            resolutions: res
-	    });
-
-		this.map = L.map('map', 
-		{
-			crs:rd
-		}).setView([51.95085,5.78016],6);
+		this.initMap(config);
 
 		this.baseLayers = this.initLayers(config.baseLayers);
 		this.layers = this.initLayers(config.layers);
@@ -62,7 +48,25 @@ function vbmap(){
 	this.initControls = function(config){
 		this.map.addControl( new L.Control.Gps({autoActive:false, maxZoom: 14, setView:true}) );
 
-	//	L.control.layers(this.baseLayers, null).addTo(this.map);
+		L.control.layers(this.baseLayers, null).addTo(this.map);
+	},
+
+	this.initMap = function(config){
+		var res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420,0.210,0.105,0.052,0.025,0.012,0.006,0.003];
+	
+		//var res = [3440.6399,1720.3199,860.1599,430.0799,215.0399,107.5199,53.7599,26.8799,13.4399,6.7199,3.3599,1.6799,0.8400,0.4200,0.2100,0.1050,0.0525,0.0262,0.0131,0.0065,0.0032,0.0016];
+		var rd = new L.Proj.CRS.TMS(
+	        'EPSG:28992',
+	        '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999], 
+	    {
+	            transformation: new L.Transformation(1,0,-1,0),
+	            resolutions: res
+	    });
+
+		this.map = L.map('map', 
+		{
+			crs:rd
+		}).setView([51.95085,5.78016],6);
 	},
 
 	/**
